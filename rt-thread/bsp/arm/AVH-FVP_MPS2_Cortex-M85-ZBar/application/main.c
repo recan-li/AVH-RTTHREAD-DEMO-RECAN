0eba58bba7dff7ecfc4ab0dd9946a44eea19cd91
recan-li <721317716@qq.com>
2024-06-06 07:35:36 +0800

    feat: Add AVH-FVP_MPS2_Cortex-M85-ZBar project

---
+++ rt-thread/bsp/arm/AVH-FVP_MPS2_Cortex-M85-ZBar/application/main.c
@@ -0,0 +1,61 @@
+/*---------------------------------------------------------------------------
+ * Copyright (c) 2021 Arm Limited (or its affiliates). All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *---------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <rtthread.h>
+
+extern int zbar_main(void);
+
+void main_thread_entry22(void *arg)
+{
+    zbar_main();
+
+    while (1) {
+        printf("22222 %s:%d\n", __func__, __LINE__);
+        rt_thread_delay(1000);
+    }
+}
+
+int main (void) 
+{   
+    int cnt = 1;
+
+    rt_thread_t tid;
+
+    tid = rt_thread_create("zbar", main_thread_entry22, RT_NULL, 4096, RT_MAIN_THREAD_PRIORITY, 20);
+    rt_thread_startup(tid);
+
+    extern int avh_rtt_debug_server_main(int argc, const char *argv[]);
+    const char *argv[] = 
+    {
+        "tcp_server",
+        "12345",
+        "12346",
+    };
+    avh_rtt_debug_server_main(3, argv);
+
+    while (1)
+    {
+        //printf("===== %d ...\n", cnt++);
+        //rt_kprintf("=====--- %d ...\n", cnt++);
+        //printf(" root\n");
+        rt_thread_delay(10000);
+    }
+
+    return 0;
+}
---
+++ rt-thread/bsp/arm/AVH-FVP_MPS2_Cortex-M85-ZBar/board/board.c
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include "rtthread.h"
+
+#include "RTE_Components.h"
+#include  CMSIS_device_header
+//#include "cmsis_os2.h"
+#ifdef RTE_Compiler_EventRecorder
+#include "EventRecorder.h"
+#endif
+
+extern int rt_hw_usart_init(void);
+
+/* SysTick configuration */
+void rt_hw_systick_init(void)
+{
+    extern uint32_t SystemCoreClock;
+    uint32_t ret = SysTick_Config(SystemCoreClock / 1000);
+    NVIC_SetPriority(SysTick_IRQn, 0xFF);
+}
+
+/**
+ * This is the timer interrupt service routine.
+ *
+ */
+void SysTick_Handler(void)
+{
+    /* enter interrupt */
+    rt_interrupt_enter();
+
+    rt_tick_increase();
+
+    /* leave interrupt */
+    rt_interrupt_leave();
+}
+
+extern int Image$$ARM_LIB_HEAP$$ZI$$Base;
+extern int Image$$ARM_LIB_STACK$$ZI$$Base;
+#define HEAP_BEGIN  ((void *)&Image$$ARM_LIB_HEAP$$ZI$$Base)
+#define HEAP_END    ((void *)&Image$$ARM_LIB_STACK$$ZI$$Base)
+
+void rt_hw_board_init(void)
+{   
+    rt_hw_systick_init();
+
+    /* Heap initialization */
+#if defined(RT_USING_HEAP)
+    rt_system_heap_init((void *)HEAP_BEGIN, (void *)HEAP_END);
+#endif 
+
+    /* Pin driver initialization is open by default */
+#ifdef RT_USING_PIN
+    //rt_hw_pin_init();
+#endif
+
+    /* USART driver initialization is open by default */
+#ifdef RT_USING_SERIAL
+    rt_hw_usart_init();
+#endif
+
+    /* Set the shell console output device */
+#if defined(RT_USING_CONSOLE) && defined(RT_USING_DEVICE)
+    rt_console_set_device(RT_CONSOLE_DEVICE_NAME);
+#endif
+
+    /* Board underlying hardware initialization */
+#ifdef RT_USING_COMPONENTS_INIT
+    rt_components_board_init();
+#endif 
+}
